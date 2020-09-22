71a5c1f2a7277801becfebbb39b35847e03edaf8
Renar Suurpere <renar.suurpere@khk.ee>
2020-09-22 11:47:09 +0300

    ylesanne 6 lahendus

---
+++ 1/yl6.py
@@ -0,0 +1,6 @@
+# kasutaja andmed
+inimesed = int(input("Sisestage inimeste arv: "))
+kohad = int(input("Sisestage kohtade arv bussis: "))
+bussid = 6
+# arvutus
+mahutavus = inimesed * kohad * bussid
