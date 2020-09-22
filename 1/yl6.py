c85696a4213133a613d1eb3376f30938c4541a24
Renar Suurpere <renar.suurpere@khk.ee>
2020-09-22 17:18:46 +0300

    Ylesanne 6 lahendus

--- 1/yl6.py
+++ 1/yl6.py
@@ -1,6 +1,10 @@
 # kasutaja andmed
 inimesed = int(input("Sisestage inimeste arv: "))
-kohad = int(input("Sisestage kohtade arv bussis: "))
-bussid = 6
+kohad = int(input("Sisestage kohtade arv: "))
+bussid = int(input("Sisestage busside arv: "))
+kohadKokku = kohad * bussid
 # arvutus
-mahutavus = inimesed * kohad * bussid
+mahutavus = min(inimesed, kohadKokku)
+jääk = inimesed - mahutavus #suurema arvudega (800 inimest, 40 kohta ja 5 bussi) jäägiga jagamine ei anna õiget tulemit
+# väljastus
+print("Kokku ootas bussi " + str(inimesed) + " inimest." " Peatusesse tuli " + str(bussid) + " bussi, igas bussis " + str(kohad) + " kohta. " "Kõikidesse bussidesse mahtus " + str(mahutavus) + " inimest." " Bussist jäi maha " + str(jääk) + " inimest.")
